3bd45c432f15e9bdbca84444064b9b89e128a4d8
rknhb <rakizunanohachan@gmail.com>
2023-03-22 10:48:40 +0900

    commit

---
+++ src/main/java/com/example/controller/CharacterController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CharacterController {
+	
+	@GetMapping("/character/{rarity}")
+	public String index() {
+		return "character/index";
+	}
+	
+	@GetMapping("/character/detail/{characterId}")
+	public String detail() {
+		return "character/detail";
+	}
+}
