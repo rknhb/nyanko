3e15fd4a0d51d3706cc3a2e082434b67b1f302ee
rknhb <rakizunanohachan@gmail.com>
2023-05-16 15:04:51 +0900

    commit

--- src/main/java/com/example/controller/admin/HomeController.java
+++ src/main/java/com/example/controller/admin/HomeController.java
@@ -1,13 +1,25 @@
 package com.example.controller.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.entity.CharactersEntity;
+import com.example.repositories.CharactersRepository;
 
 @Controller("adminhomecontroller")
 public class HomeController {
 	
+	@Autowired
+	private CharactersRepository charactersRepository;
+	
 	@GetMapping("/admin")
-	String index() {
+	String index(Model model) {
+		List<CharactersEntity> characters = charactersRepository.findAll();
+		model.addAttribute("characters", characters);
 		return "admin/index";
 	}
 }
